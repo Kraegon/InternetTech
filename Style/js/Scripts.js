da8859b71c65a5f418bf98a08aec863f0d0b5ef7
Jelle Braat <Jelle@Braat.nu>
2015-02-09 21:26:41 +0100

    added audio

---
+++ Style/js/Scripts.js
@@ -0,0 +1,39 @@
+/*!
+ *  v0.1
+ *  By Julian G. West & Jelle Braat
+ *  Custom Scripts
+*/
+function ChangeCss(documentPage){
+	if (documentPage == "index"){
+		if (document.getElementById("style").getAttribute("href") == "Style/css/MasterSheet.css")
+		{
+			document.getElementById("style").setAttribute("href", "Style/css/MetroSheet.css");
+		}
+		else
+		{
+			document.getElementById("style").setAttribute("href", "Style/css/MasterSheet.css");
+		}
+	}
+	else 
+	{
+		if (document.getElementById("style").getAttribute("href") == "../Style/css/MasterSheet.css")
+		{
+			document.getElementById("style").setAttribute("href", "../Style/css/MetroSheet.css");
+		}
+		else
+		{
+			document.getElementById("style").setAttribute("href", "../Style/css/MasterSheet.css");
+		}
+	}
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+} 
+
+
+
+
