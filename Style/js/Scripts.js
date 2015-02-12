c5a3029146a874360fee35e2a343e0ec0105be26
Jelle Braat <Jelle@Braat.nu>
2015-02-12 11:08:56 +0100

    updated style & add fieldset + legend

--- Style/js/Scripts.js
+++ Style/js/Scripts.js
@@ -8,10 +8,12 @@ function ChangeCss(documentPage){
 		if (document.getElementById("style").getAttribute("href") == "Style/css/MasterSheet.css")
 		{
 			document.getElementById("style").setAttribute("href", "Style/css/MetroSheet.css");
+			makeCookieTTLOneDay("Style", "MetroSheet.css");
 		}
 		else
 		{
 			document.getElementById("style").setAttribute("href", "Style/css/MasterSheet.css");
+			makeCookieTTLOneDay("Style", "MasterSheet.css");
 		}
 	}
 	else 
@@ -19,12 +21,19 @@ function ChangeCss(documentPage){
 		if (document.getElementById("style").getAttribute("href") == "../Style/css/MasterSheet.css")
 		{
 			document.getElementById("style").setAttribute("href", "../Style/css/MetroSheet.css");
+			makeCookieTTLOneDay("Style", "MetroSheet.css");
 		}
 		else
 		{
 			document.getElementById("style").setAttribute("href", "../Style/css/MasterSheet.css");
+			makeCookieTTLOneDay("Style", "MasterSheet.css");
 		}
 	}
+}
+
+function makeCookieTTLOneDay(name, value){
+
+
 }
 
 function setCookie(cname, cvalue, exdays) {
@@ -32,8 +41,22 @@ function setCookie(cname, cvalue, exdays) {
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+d.toUTCString();
     document.cookie = cname + "=" + cvalue + "; " + expires;
-} 
+}
 
+function getCookie(cookieName){
+    var name = cookieName + "=";
+    var cookieList = document.cookie.split(';');
+    for(var i=0; i<cookieList.length; i++) {
+        var c = cookieList[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+function checkStyleCookie(){
+
+}
 
 
 
